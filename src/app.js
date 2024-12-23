d1c28dc21ddfbb6266e89e09be92a2023f97b771
MrPotatoJr <90863275+MrPotatoJr@users.noreply.github.com>
2024-12-23 02:12:14 +0000

    Domain

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable */
+/* eslint-disable 
 import "bootstrap";
 import "./style.css";
 
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+*/
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big','skibiddi'];
+let noun = ['jogger', 'racoon','toilet'];
+let dom = ['.com','.es','.mx'];
+
+for(let i = 0; i < pronoun.length; i++){
+  for(let j = 0; j < adj.length; j++){
+    for (let k = 0; k < noun.length; k++){
+      for(let m = 0; m < dom.length; m++)
+      {
+    
+    console.log(adj[j]+ pronoun[i] + noun[k] + dom[m] );
+  }
+}}}
